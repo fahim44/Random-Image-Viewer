8318fec523abf28fe4f3a4f57ef254e332d5fc28
Fahim Salam Chowdhury <fahim.cse11@gmail.com>
2020-12-04 09:28:32 +0600

    Initialize project
    Setup basic hello-world native android project.

---
+++ app/src/main/java/com/lamonjush/random_image_viewer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lamonjush.random_image_viewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
